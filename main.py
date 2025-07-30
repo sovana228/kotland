2951f41cd1e60595ecdddf755002e7e65c1bd700
sovana228 <soloberti456@gmail.com>
2025-07-30 11:32:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot("7812854680:AAHvmQHc_lyPJ3OpVMna-JX_Jzsv8jIlEmE")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin /donlo ")
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin /photo ")
 
 @bot.message_handler(commands=['hello'])
 def send_hello(message):
@@ -30,35 +30,38 @@ def send_emodji(message):
 @bot.message_handler(commands=['coin'])
 def send_coin(message):
     coin = flip_coin()
-    bot.reply_to(message, f"Монетка выпала так: {coin}")
+    bot.reply_to(message, f"Монетка выпала так: {coin}")   
 
-@bot.message_handler(content_types=["photo"])  # Исправлено "donlo" на "photo"
-def send_donlo(message):
+@bot.message_handler(content_types=["photo"])
+def send_photo(message):
+    # Проверка наличия фото
     if not message.photo:
-        return bot.send_message(message.chat.id, "Вы не загрузили картинку")  # Добавлен .chat.id
+        return bot.reply_to(message, "Вы не загрузили картинку")
     
+    # Скачивание и обработка фото
     file_info = bot.get_file(message.photo[-1].file_id)
     file_name = file_info.file_path.split('/')[-1] 
-
     downloaded_file = bot.download_file(file_info.file_path)
+    
+    # Сохранение файла
     with open(file_name, 'wb') as new_file:
         new_file.write(downloaded_file)
-
-    result = get_class(file_name)
-    if result == 'minecraft':
-        bot.send_message(message, 'Значит ты в маин играешь, попробуй поиграть с друзьями, так будет веселее')  # Добавлен .chat.id и форматирование текста
-
-    if result == 'age of history':  # Исправлено опечатку в "age os history"
-        bot.send_message(message, 'О, ты играешь, получается. В это советую создать там СССР за Приднестровье')  # Добавлен .chat.id и форматирование
-
-    if result == 'roblox':
-        bot.send_message(message, 'Ты играешь в Роблокс? Если да и любишь хойку, то заходи в режим Control Europe')  # Добавлен .chat.id и форматирование
     
-    bot.send_message(message, result)  # Добавлен .chat.id
+    # Анализ изображения
+    result = get_class(file_name)
+    
+    # Ответы в зависимости от результата
+    if result == 'minecraft':
+        bot.reply_to(message, 'Привет это бот Soloberty.Значит ты в маин играешь, попробуй поиграть с друзьями, так будет веселее.Minecraft это игра где можно строить развиваться играть с кемто.')
+    elif result == 'ageofhistory':
+        bot.reply_to(message, 'Привет это бот Soloberty.О, ты играешь, получается в AOC2. В это советую создать там СССР за Приднестровье.AOC2 это игра атратегия там ты развиваешь страну и захватываешь мир')
+    elif result == 'roblox':
+        bot.reply_to(message, 'Привет это бот Soloberty.Ты играешь в Роблокс? Если да и любишь хойку, то заходи в режим Control Europe.Роблокс это платформа где ты выбераишь в какие игры или бби поиграть там есть хорроры стратегии и др...')
+    
+    bot.send_message(message.chat.id, result)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
-
 # Запускаем бота
 bot.polling()
